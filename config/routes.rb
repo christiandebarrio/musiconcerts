6af163b2d093474abe93fdf856d077b766b829ac
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-20 15:09:09 +0100

    links in concerts list

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :concerts, only:[:index, :new, :create]   
+  resources :concerts, only:[:index, :new, :create, :show]   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
