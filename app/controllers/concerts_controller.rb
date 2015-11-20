e8db46b661209fa5fe36129f1dac44967c4fc040
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-20 10:28:17 +0100

    index concerts

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,10 +1,26 @@
 class ConcertsController < ApplicationController
 
+  def index
+    @concerts = Concert.all
+  end
+
   def new
     @concert = Concert.new
   end
 
   def create
-    
+    @concert = Concert.new(concert_params)
+
+    if @concert.save
+      redirect_to concerts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def concert_params
+    params.require(:concert).permit(:band, :venue, :city, :date, :price, :description)
   end
 end
