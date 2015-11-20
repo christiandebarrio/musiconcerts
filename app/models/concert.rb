200126bb4472493e35d25b4ea09bce7c698025c7
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-20 14:56:10 +0100

    migrate comments

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,4 +1,5 @@
 class Concert < ActiveRecord::Base
+  has_many :comments
   
   validates :band, 
             :venue, 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120083424) do
+ActiveRecord::Schema.define(version: 20151120135202) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "concerts", force: :cascade do |t|
     t.string   "band"
